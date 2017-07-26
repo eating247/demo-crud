0e052677869f88b5ae9d7292d491eef3226a042f
tingting <ttwang.303@gmail.com>
2017-07-26 17:03:13 -0400

    Add edit action and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,10 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   private
 
   def article_params
